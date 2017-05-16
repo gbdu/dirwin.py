bc5336be7fd0361394e72d654ad9fbd71b4bfdd8
gbdu <ogrum@live.com>
2017-05-16 11:30:36 -0600

    restruct

---
+++ README.md
@@ -0,0 +1,19 @@
+# dirwin.py
+* Display and 
+
+[![N|Solid](/screenshot.png)]("a")
+
+dirwin.py is a way to navigate and display window layouts while preserving the z buffer.
+
+# Basic usuage:
+- ./dirwin.py list # Show current window layout as ascii
+- ./dirwin.py left  # focus window to the left of this one
+
+# Directional movement:
+- Directional movement is a pretty useful feature in dynamic and tiling window managers, if like me you still prefer WM's like Openbox this script is a way to get that functionality.
+
+# Dependencies:
+The following binaries must be in your path:
+- xprop
+- xwininfo
+- wmctrl
---
+++ dirwin.py
@@ -0,0 +1,274 @@
+#!/usr/bin/python3
+
+# Requires: xprop, xwininfo
+import sys
+import os
+import subprocess
+import re
+
+from collections import Counter
+import operator
+
+# Config section:
+desktop_geo = None
+scale = 32
+lastchar='A'
+# end of config section
+
+known_symbols = {}
+
+def get_active_window():
+    cmd = ["xdotool", "getactivewindow"]
+
+    decoded = subprocess.check_output(cmd).decode("utf-8")
+    
+    return int(decoded)
+
+def parse_winfo(linfo):
+    dinfo = {}
+    for i in linfo:
+        if not i: continue
+        
+        nline = i.replace(" ","")
+        line = nline.replace("xwininfo:","")        
+
+        if line.startswith("Windowid"):
+            ep = line.find('\"')
+            if ep == -1:
+                print ("ERROR, wrong value passed from xwininfo")
+                return None
+            else:
+                wid = line[9:ep]
+                title = line[ep:].replace("\"","")
+                title = title.replace("\'", "")
+                dinfo.update(title=title)
+                dinfo.update(wid=wid)
+                
+        else:
+            el = i.strip().lower()
+            if el.startswith("corners"):
+                corners=(el[8:].split())
+                dinfo.update(corners=corners)
+            else:
+                el = nline
+                el = el.replace("r-l", "rl")
+                el = el.replace("-geometry", "mgeometry:")
+                el = el.lower()
+                
+                keys = ["absoluteupperleftx", "absoluteupperlefty", \
+                		"relativeupperleftx", "relativeupperlefty", \
+                		"width", "height"]
+
+                
+                for k in keys :
+                	if el.startswith(k):
+                		val= el[len(k)+1:]
+                		dinfo.update({k:int(val)})
+
+                
+                   
+    return dinfo
+
+def get_client_stack():
+    cmd = ["xprop -root"]
+
+    proc = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
+
+    while True:
+        line = proc.stdout.readline()
+        if line != '':
+            lo = line.decode("utf-8")
+            global desktop_geo
+            if desktop_geo == None and lo.startswith("_NET_DESKTOP_GEOMETRY(CARDINAL)"):
+                g = lo[lo.find("=")+2:]
+                g = g.replace(" ", "")
+                g = g.split(",")
+
+                desktop_geo = (int(g[0]), int(g[1])) 
+            elif lo.startswith("_NET_CLIENT_LIST_STACKING"):
+                lo = lo[lo.find("#") + 1:]
+                windows = [x.strip() for x in lo.split(",")]
+                if desktop_geo : return windows
+        else:
+            break
+
+
+def get_window_info(win):
+    cmd = ["xwininfo", "-id", "%s" % win]
+
+    minfo = {}
+
+    decoded = subprocess.check_output(cmd).decode("utf-8")
+
+    winfo_as_list = decoded.split('\n')
+    return parse_winfo(winfo_as_list)
+
+
+def create_test_buffer(d=None):
+    if not d:
+        d = desktop_geo
+
+    global scale
+    #scale = 1
+    w = int(d[0]/scale)
+    h = int(d[1]/scale)
+
+    print(w,h)
+    print("-" * 60)
+    print()
+
+    buffer = [ [ '0' for x in range(h) ] for y in range(w)]
+    return buffer
+
+def populate_buffer(b,winfos):
+	# b is a list of columns (which are lists of chars)
+    
+    # scan each window
+    for i in winfos:
+        for col in range(int(i["absoluteupperleftx"]/scale), int(i["absoluteupperleftx"]/scale+i["width"]/scale)):
+        	for row in range(int(i["absoluteupperlefty"]/scale), int(i["absoluteupperlefty"]/scale+i["height"]/scale)):
+        		# print (col,row)
+        		try:
+        			b[col-1][row-1] = i['char']
+        		except IndexError:
+        			print("out of range (%d,%d)" % (col-1, row-1))
+        			exit(1)
+selected_char = ""
+
+def get_char_for(wid):
+    global lastchar
+    global known_symbols
+    global selected_char
+    
+    curr = lastchar
+    lastchar = chr(ord(lastchar)+1)
+    known_symbols.update({curr:wid})
+
+    
+    return curr 
+    
+stack = get_client_stack()
+infos = []
+
+for i in stack:
+    winfo = get_window_info(i)
+    if(winfo["title"]=="xfce4-panel"):
+        continue
+    
+    infos.append(winfo)
+
+#infos = infos[::-1]
+
+for i in infos:
+    i['char'] = get_char_for(i["wid"])
+    w = int(i['wid'], 16);
+    
+    if w == get_active_window():
+        i['is_selected'] = 1
+        selected_char = i['char']
+    else:
+        i['is_selected'] = 0
+
+#print(infos)
+#print(desktop_geo)
+
+buffer = create_test_buffer()
+populate_buffer(buffer, infos)
+
+def calc_affinity(l):
+    d = Counter(l)
+    so = sorted(d.items(), key=lambda x: x[1])
+    if so:
+        target_symbol = ((so[len(so)-1]))[0]
+    else:
+        return 
+        #print(mydict.keys()[mydict.values().index(16)] # Prints george
+    for k,v in known_symbols.items():
+        if k == target_symbol:
+            return v;
+    
+    
+def window_char_up(startchar):
+    for row in range(0,len(buffer[0])):
+        bs=0
+        hits=[]
+        for col in range(0,len(buffer)):
+            if(buffer[col][row] == startchar):
+                #print("%c at (%d,%d)\n" % (startchar,row,col))
+                
+                for j in reversed(range(0,row)):
+                    if buffer[col][j] == '0':
+                        continue;
+                    if buffer[col][j] != startchar:
+                        hits.append(buffer[col][j])
+                        break
+                bs=1
+                
+        if hits:
+            print(hits)
+            return hits
+        if bs:
+            break;
+
+def window_char_down(startchar):
+    for row in reversed(range(0,len(buffer[0]))):
+        bs=0
+        hits=[]
+        for col in range(0,len(buffer)):
+            if(buffer[col][row] == startchar):
+                #print("%c at (%d,%d)\n" % (startchar,row,col))
+                
+                for j in range(row, len(buffer[0])):
+                    if buffer[col][j] == '0':
+                        continue;
+                    if buffer[col][j] != startchar:
+                        hits.append(buffer[col][j])
+                        break
+                bs=1
+                
+        if hits:
+            print(hits)
+            return hits
+        if bs:
+            break;
+
+    
+def print_buff(buff):
+    sbuff = ""
+    for row in range(0,len(buff[0])):
+        for col in range(0,len(buff)):
+        #thefile.write("%c" % buff[col][row])
+            sbuff += str(buff[col][row])
+        sbuff += "\n"
+    
+    print(sbuff)
+
+if len(sys.argv) == 1:
+    exit;
+
+def transposed(lists):
+   if not lists: return []
+   return list(map(lambda *row: list(row), *lists))
+
+if sys.argv[1]=="up":
+    target_window_id = calc_affinity(window_char_up(selected_char))
+elif sys.argv[1]=="down":
+    target_window_id = calc_affinity(window_char_down(selected_char))
+elif sys.argv[1]=="left":
+    buffer = transposed(buffer)
+    target_window_id = calc_affinity(window_char_up(selected_char))
+elif sys.argv[1]=="right":
+    buffer = transposed(buffer)
+    target_window_id = calc_affinity(window_char_down(selected_char))
+elif sys.argv[1]=="show":
+    print_buff(buffer)
+    exit(0)
+else:
+    print("unknown argument")
+    exit(1)
+
+if target_window_id:
+    cmd = ["wmctrl", "-i", "-a", target_window_id]
+    decoded = subprocess.check_output(cmd).decode("utf-8")
+    print(decoded)
+
